8fdf1944efc6c6944dfda720c32e03b881ead2cc
Sam Olyver <samuel0733@hotmail.com>
2016-12-26 16:34:24 -0200

    projeto em estágio inicial, foi removido o html para reestruturação

---
+++ js/script-banner.js
@@ -0,0 +1,22 @@
+var $ = jQuery.noConflict();
+		$(window).load(function() {
+		$('.flexslider').flexslider({
+			  animation: "fade"
+		});
+		
+		$(function() {
+			$('.show_menu').click(function(){
+					$('.menu').fadeIn();
+					$('.show_menu').fadeOut();
+					$('.hide_menu').fadeIn();
+			});
+			$('.hide_menu').click(function(){
+					$('.menu').fadeOut();
+					$('.show_menu').fadeIn();
+					$('.hide_menu').fadeOut();
+			});
+		});
+	  });
+	
+		
+			
